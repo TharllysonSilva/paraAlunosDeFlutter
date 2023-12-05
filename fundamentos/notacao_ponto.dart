5749503e87c66bb26689449b9c4151b53459fc17
Tarllysson Silva <tharllysonsilva@gmail.com>
2023-12-05 17:57:09 -0300

    notacao de ponto no dart

--- fundamentos/notacao_ponto.dart
+++ fundamentos/notacao_ponto.dart
@@ -1,5 +1,8 @@
 main() {
   //double nota = 6.99.roundToDouble();
-  double nota = 6.99.truncateToDouble();
-  print(nota);
+  // double nota = 6.99.truncateToDouble();
+  // print(nota);
+
+  var s1 = 'Tarllysson André'.substring(0, 10).toUpperCase().padRight(13, '!');
+  print(s1);
 }
