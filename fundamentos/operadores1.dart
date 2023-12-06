50b15dd29a7652ea057378b846babe1d4d125126
Tarllysson Silva <tharllysonsilva@gmail.com>
2023-12-06 18:47:56 -0300

    Operadores aula 1

---
+++ fundamentos/operadores1.dart
@@ -0,0 +1,20 @@
+main() {
+//Operadores Aritméticos (binários/infix)
+
+  int a = 7;
+  int b = 3;
+
+  int resultado = a + b;
+  print(resultado);
+  print(a * b);
+  print(a + b);
+  print(a - b);
+  print(a % b);
+
+//Operadores Lógicos
+
+  bool produtoFragil = true;
+  bool produtoCaro = false;
+
+  print(produtoFragil && produtoCaro);
+}
