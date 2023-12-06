ea9f2aeabe2390e4e9c4ceb694dd6a3ecd5437cf
Tarllysson Silva <tharllysonsilva@gmail.com>
2023-12-06 19:19:11 -0300

    Operadores de Atribuição e Relacionais

---
+++ fundamentos/operadores2.dart
@@ -0,0 +1,21 @@
+main() {
+//Operadores Atribuição (binários/infix)
+  double a = 2;
+  a = a + 3;
+  a += 3;
+  a -= 3;
+  a *= 3;
+  a /= 5;
+  a %= 2;
+
+  print(a);
+
+//Operadores Relacionais (binários/infix) -> O resultado sempre será BOOL
+
+  print(3 > 2);
+  print(3 >= 3);
+  print(3 < 4);
+  print(3 <= 3);
+  print(3 != 3);
+  print(3 == 3);
+}
