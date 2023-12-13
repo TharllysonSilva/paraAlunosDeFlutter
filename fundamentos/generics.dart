639ade146bd53cd78b5892fe98565869b285655c
Tarllysson Silva <tharllysonsilva@gmail.com>
2023-12-13 20:09:50 -0300

    generics

--- fundamentos/generics.dart
+++ fundamentos/generics.dart
@@ -3,4 +3,12 @@ main() {
   frutas.add('melão');
 
   print(frutas);
+
+  Map<String, double> salarios = {
+    'gerente': 19650.78,
+    'vendedor': 16234.80,
+    'estagio': 1000.00,
+  };
+
+  print(salarios);
 }
