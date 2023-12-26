c912fbcf789f299a08efa55c49643e940a348973
Tarllysson Silva <tharllysonsilva@gmail.com>
2023-12-26 18:49:35 -0300

    desafio para os alunos

---
+++ controle/desafio_if_else.dart
@@ -0,0 +1,11 @@
+main() {
+  var nota = 2.3;
+  if (nota >= 9.0) // NÃO USA ; EM ESTRUTURAS DE CONTROLE *
+  {
+    print('Parabéns! Você é um aluno brilhante!!!');
+  } else {
+    print('Tente próximo ano');
+  }
+
+  //COM EXECEÇÃO O DO/WHILE *
+}
