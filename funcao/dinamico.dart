e137e7346ce44db44e82d251043ed8f5907de0ca
Tarllysson Silva <tharllysonsilva@gmail.com>
2024-02-28 17:08:41 -0300

    Uma explicação previa de dynamic

---
+++ funcao/dinamico.dart
@@ -0,0 +1,11 @@
+main() {
+  juntar(1, 7);
+  juntar('Boa ', 'Tarde !!!');
+  String resultado = juntar('O valor do salario de um dev é ', 8.500);
+  print(resultado);
+}
+
+juntar(dynamic a, b) {
+  print(a.toString() + b.toString());
+  return a.toString() + b.toString();
+}
